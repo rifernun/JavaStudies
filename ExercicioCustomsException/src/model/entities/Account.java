6a04b1e1e9587039e2ac18a50fe81fefb88d041d
Richard Nunes <rifernun@gmail.com>
2026-02-03 16:59:27 -0300

    feat: Adding CustomsExceptionExercise

---
+++ ExercicioCustomsException/src/model/entities/Account.java
@@ -0,0 +1,62 @@
+package model.entities;
+
+import model.exceptions.BusinessException;
+
+public class Account {
+    private Integer number;
+    private String holder;
+    private Double balance;
+    private Double withDrawLimit;
+
+    public Account(){}
+
+    public Account(Integer number, String holder, Double balance, Double withDrawLimit) {
+        this.number = number;
+        this.holder = holder;
+        this.balance = balance;
+        this.withDrawLimit = withDrawLimit;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public String getHolder() {
+        return holder;
+    }
+
+    public void setHolder(String holder) {
+        this.holder = holder;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+
+    public Double getWithDrawLimit() {
+        return withDrawLimit;
+    }
+
+    public void setWithDrawLimit(Double withDrawLimit) {
+        this.withDrawLimit = withDrawLimit;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withDraw(double amount) {
+        if(amount > getWithDrawLimit()){
+            throw new BusinessException("Error: The amount exceeds withdraw");
+        }
+        if(amount > getBalance()) {
+            throw new BusinessException("Error: not enough balance");
+        }
+        balance -= amount;
+    }
+}
